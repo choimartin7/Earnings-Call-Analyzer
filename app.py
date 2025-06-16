352e9ad9b3970592a068175d7af6f2d30ed0c662
choimartin7 <choimartin7@gmail.com>
2025-06-17 01:55:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -261,8 +261,6 @@ def predict():
     except Exception as e:
         logger.error(f"Error making prediction: {e}")
         return jsonify({'error': f'Prediction failed: {str(e)}'}), 500
-
-port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
-app.run(debug=True, host='0.0.0.0', port=port)
-
-
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
+    app.run(debug=True, host='0.0.0.0', port=port)
